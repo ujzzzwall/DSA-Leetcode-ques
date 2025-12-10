49c8075c2c9739978504f5ef5bc9e326e4455de1
Ujjwal singh <ujzzzwall@gmail.com>
2025-12-10 12:33:32 +0530

    Time: 7 ms (18.65%), Space: 11.5 MB (67.53%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string predictPartyVictory(string s) {
+        queue<int>q;
+        queue<int>r;
+        queue<int>d;
+        int n =s.length();
+        for(int i =0 ; i<n ;i++){
+            q.push(i);
+            if(s[i]=='R')r.push(i);
+            else d.push(i);
+        }
+        while(q.size()>1){
+            if(s[q.front()]=='X')q.pop();
+            else if(s[q.front()]=='R'){
+                if(d.size()==0)return "Radiant";
+                else{
+                    s[d.front()] = 'X';
+                    d.pop();
+                    q.push(q.front());
+                    q.pop();
+                    r.push(r.front());
+                    r.pop();
+                }
+            }
+            else {
+                if(r.size()==0)return "Dire";
+                else{
+                    s[r.front()] = 'X';
+                    r.pop();
+                    q.push(q.front());
+                    q.pop();
+                    d.push(d.front());
+                    d.pop();
+                }
+            }
+        }
+        if(s[q.front()]=='R') return "Radiant";
+        else return "Dire";
+    }
+};
